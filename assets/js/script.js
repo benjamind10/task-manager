7ca92c872f996c3946a91c560c15a32c18d96c94
Ben <benjamind10@pm.me>
2021-12-31 11:05:17 -0500

    added index to timeblocks for looping localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,31 @@
 var currentHour = moment().hours();
 var currentDate =
   moment().format('dddd') + ' ' + moment().format('Do MMM YYYY');
+var uInput;
+var indexId;
 
 // Function that makes an array with all the time blocks
 function makeArr() {
   var arr = [];
   arr = $('textarea').map((j, element) => {
     return $(element);
+  });
+  return arr;
+}
+
+function startDay(arr) {
+  arr.each(function (i) {
+    console.log(arr[i]);
+  });
+}
+
+function saveBtn() {
+  $('.saveBtn').on('click', function () {
+    userInput = $(this).siblings('.form-control').val().trim();
+    console.log(userInput);
+    indexId = $(this).attr('id');
+    console.log(indexId);
+    localStorage.setItem(indexId, JSON.stringify(userInput));
   });
 }
 
@@ -42,4 +61,5 @@ function setBackground() {
 // This is the functiont hat runs at the beginning of the app
 $(function () {
   setBackground();
+  saveBtn();
 });
